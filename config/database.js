7fee19d324758513b52655527c9b8adc4148c12f
rthompson17 <ryann.thompson@gmail.com>
2022-05-22 11:33:20 -0400

    config updated to match assignment

--- config/database.js
+++ config/database.js
@@ -10,3 +10,7 @@ const db = mongoose.connection;
 db.on("connected", function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
+
+db.on('error', function(err){
+  console.log(`Mongodb error: ${err}`)
+})
