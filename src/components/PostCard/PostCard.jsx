55f1c86b4c4dd33bcf69baa3944d64bb5ad56b51
rthompson17 <ryann.thompson@gmail.com>
2022-05-22 13:17:01 -0400

    PostCard component written

---
+++ src/components/PostCard/PostCard.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+
+function PostCard({ post, isProfile, removeLike, addLike, user }) {
+    const likeIndex = post.likes.findIndex(
+        (like) => like.username === user.username
+    );
+
+    const clickHandler = 
+      likeIndex > -1
+        ? () => removeLike(post.likes[likeIndex]._id)
+        : () => addLike(post._id);
+
+    const likeColor = likeIndex > -1 ? "red" : "grey";
+
+    return (
+        <Card key={post._id} raised>
+            {isProfile ? (
+                ""
+            ) : (
+              <Card.Content textAlign="left">
+                  <Card.Header>
+                      <Link to={`/${post.user.username}`}>
+                          <Image
+                            size="large"
+                            avatar
+                            src={
+                              post.user.photoUrl
+                                ? post.user.photoUrl
+                                : "https://react.semantic-ui.com/images/wireframe/square-image.png"
+                            }
+                          />
+                          {post.user.username}
+                      </Link>
+                  </Card.Header>
+              </Card.Content>
+            )}
+
+            <Image src={`${post.photoUrl}`} wrapped ui={false} />
+            <Card.Content>
+                <Card.Description>{post.caption}</Card.Description>
+            </Card.Content>
+            <Card.Content extra textAlign={"right"}>
+                <Icon
+                name={"heart"}
+                size="large"
+                color={likeColor}
+                onClick={clickHandler}
+            />
+            {post.likes.length} Likes
+            </Card.Content>
+        </Card>
+    );
+}
+
+export default PostCard;
